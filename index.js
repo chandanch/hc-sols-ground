f9b85afd71f279af07f3a903601a59fc68fe5c1a
chandanch <chandanch@users.noreply.github.com>
2023-07-18 23:59:59 +0530

    string_xor implementation

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-function plusMinus(arr) {
-  // Write your code here
-  const ratioConter = {
-    postiveCount: 0,
-    negativeCount: 0,
-    zeroCount: 0,
-  };
-  arr.forEach((value) => {
-    if (value > 0) {
-      ratioConter.postiveCount += 1;
-    } else if (value < 0) {
-      ratioConter.negativeCount += 1;
-    } else {
-      ratioConter.zeroCount += 1;
+function strings_xor(firstString, secondString) {
+  const firstArr = firstString.split('');
+  const secondArr = secondString.split('');
+  const resultArr = [];
+
+  firstArr.forEach((element, index) => {
+    if (element === secondArr[index]) {
+      resultArr.push(0);
+    } else if (element || secondArr[index]) {
+      resultArr.push(1);
     }
   });
-
-  console.log(
-    `${(ratioConter.postiveCount / arr.length).toFixed(6)}\n ${(
-      ratioConter.negativeCount / arr.length
-    ).toFixed(6)}\n ${(ratioConter.zeroCount / arr.length).toFixed(6)}\n`
-  );
+  return resultArr.join('');
 }
 
-const res = plusMinus([1, 2, -1, -3, 0, 3]);
+const res = strings_xor('101010', '001101');
+
+console.log(res);
