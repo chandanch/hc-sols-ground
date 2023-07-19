79a54f80422fa08d47fe87ab8239738fe7b6b136
chandanch <chandanch@users.noreply.github.com>
2023-07-19 12:59:18 +0530

    migratory bird occurence

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-function strings_xor(firstString, secondString) {
-  const firstArr = firstString.split('');
-  const secondArr = secondString.split('');
-  const resultArr = [];
-
-  firstArr.forEach((element, index) => {
-    if (element === secondArr[index]) {
-      resultArr.push(0);
-    } else if (element || secondArr[index]) {
-      resultArr.push(1);
-    }
+function migratoryBirds(arr) {
+  // Write your code here
+  const coutTracker = {};
+  arr.forEach((item) => {
+    coutTracker[item] ? (coutTracker[item] += 1) : (coutTracker[item] = 1);
   });
-  return resultArr.join('');
+
+  const maxOccurence = Math.max(...Object.values(coutTracker));
+
+  const maxOccuredId = Math.min(
+    ...Object.keys(coutTracker).filter(
+      (key) => coutTracker[key] === maxOccurence
+    )
+  );
+
+  return maxOccuredId;
 }
 
-const res = strings_xor('101010', '001101');
+const res = migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4, 5, 5, 5, 4, 5, 4]);
 
 console.log(res);
