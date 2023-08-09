abc2522d0da96d2033e87875bf486f716727b810
chandanch <chandanch@users.noreply.github.com>
2023-08-09 23:08:03 +0530

    pagecount changes

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ function pageCount(n, p) {
   let pageTracker = 0;
   let flipCount = 0;
 
+  if (p > n) {
+    return -1;
+  }
+
   for (let i = 0; i <= p; i++) {
     if (i === 0) {
       pageTracker += 1;
+      if (pageTracker === p) {
+        break;
+      }
     } else {
       pageTracker += 1;
       if (pageTracker === p) {
@@ -25,5 +32,5 @@ function pageCount(n, p) {
   return flipCount;
 }
 
-const res = pageCount(6, 2);
+const res = pageCount(6, 10);
 console.log(res);
